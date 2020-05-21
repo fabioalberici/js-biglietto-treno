4c2d3021e0e8db8b2504a12ef0f36e16b73288f2
fabioalberici <fabioalbe79@gmail.com>
2020-05-21 10:36:50 +0200

    non riesco a collegare e/o vedere java sul mio browser..
    qualsiasi cosa faccia non riesco a vederla, ne con console.log, che con i prompt, ci sarà un errore di collegamento, ma non riesco a capire dove

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//domande iniziali d'informazione
+var etàCliente = parseInt(prompt ('Quanti anni hai?'));
+var kmCliente = prompt ('Quanti km devi percorrere?');
+
+console.log(kmCliente);
+console.log(etàCliente);
